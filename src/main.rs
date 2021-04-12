d02933a332f729eb2fd57bc0c0087c61a19923b0
Luiz Carlos Mourão Paes de Carvalho <luizcarlosmpc@gmail.com>
2021-04-11 23:11:48 -0300

    Caching resolved addresses

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,62 @@
 mod ext;
 
-use std::{io, net::SocketAddr};
+use std::{io, net::SocketAddr, sync::Arc};
 
 use tokio::{io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt}, net::TcpStream};
 
-lazy_static::lazy_static!{
-    static ref TARGET_ADDR: String = std::env::var("TARGET_ADDR").expect("missing TARGET_ADDR env");
+pub struct Server {
+    target_addrs: Vec<SocketAddr>,
+    target_raw_addr: String,
+    target_raw_port: u16,
 }
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let addr = std::env::var("TARGET_ADDR").expect("missing TARGET_ADDR env");
+    let (raw_addr, raw_port) = parse_addr(&addr);
+    
+    let server = Arc::new(Server {
+        target_addrs: tokio::net::lookup_host(&addr).await?.collect(),
+        target_raw_addr: raw_addr,
+        target_raw_port: raw_port,
+    });
+
     let listener = tokio::net::TcpListener::bind(
         std::env::var("BIND_ADDR").unwrap_or("0.0.0.0:25565".into())
     ).await?;
 
+    println!("listening for connection on {}", listener.local_addr()?);
+
     while let Ok((stream, addr)) = listener.accept().await {
-        tokio::spawn(handle_connection(stream, addr));
+        let server = server.clone();
+        tokio::spawn(handle_connection(server, stream, addr));
     }
 
     Ok(())
 }
 
-async fn handle_connection(stream: TcpStream, addr: SocketAddr) {
+fn parse_addr(addr: &str) -> (String, u16) {
+    let mut split = addr.splitn(2, ':');
+
+    (
+        split.next().expect("invalid target addr").into(), 
+        split.next().map(|port| port.parse().expect("invalid port")).unwrap_or(25565u16)
+    )
+}
+
+async fn handle_connection(server: Arc<Server>, stream: TcpStream, addr: SocketAddr) {
     let (mut c_reader, mut c_writer) = tokio::io::split(stream);
 
-    let server = match TcpStream::connect(TARGET_ADDR.as_str()).await {
+    let server_stream = match TcpStream::connect(&server.target_addrs[..]).await {
         Ok(stream) => stream,
         Err(err) => {
             eprintln!("{}: Failed to connect to target server: {}", addr, err);
             return;
         },
     };
-    let server_addr = match server.peer_addr() {
-        Ok(addr) => addr,
-        Err(err) => {
-            eprintln!("{}: Failed to get remote addr: {}", addr, err);
-            return;
-        },
-    };
-    let (mut s_reader, mut s_writer) = tokio::io::split(server);
+    let (mut s_reader, mut s_writer) = tokio::io::split(server_stream);
 
-    match handle_handshake(&mut c_reader, &mut s_writer, &server_addr).await {
+    match handle_handshake(&server, &addr, &mut c_reader, &mut s_writer).await {
         Err(err) => {
             eprintln!("{}: Failed to handle handshake: {}", addr, err);
             return;
@@ -61,7 +77,7 @@ async fn handle_connection(stream: TcpStream, addr: SocketAddr) {
     }
 }
 
-async fn handle_handshake<R, W>(c_reader: &mut R, s_writer: &mut W, remote_addr: &SocketAddr) -> io::Result<()>
+async fn handle_handshake<R, W>(server: &Arc<Server>, addr: &SocketAddr, c_reader: &mut R, s_writer: &mut W) -> io::Result<()>
 where
     R: AsyncRead + Unpin,
     W: AsyncWrite + Unpin,
@@ -80,15 +96,18 @@ where
         format!("invalid handshake packet")
     ))?;
 
-    let addr = TARGET_ADDR.clone();
-    let host = addr.split(':').next().unwrap().as_bytes();
+    if next_state == 2 {
+        println!("{}: is logging in...", addr);
+    }
+
+    let host = server.target_raw_addr.as_bytes();
 
     let mut payload = Vec::with_capacity(6 + host.len());
     payload.push(0);
     payload.push(47);
     payload.push(host.len() as u8);
     payload.extend_from_slice(host);
-    payload.extend_from_slice(&remote_addr.port().to_be_bytes());
+    payload.extend_from_slice(&server.target_raw_port.to_be_bytes());
     payload.push(next_state);
 
     ext::write_varint(payload.len() as i32, s_writer).await?;
